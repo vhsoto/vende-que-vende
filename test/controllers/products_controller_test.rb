880a6706d089aae63fb03385dce70d53adeaa8e7
vhsoto <vhsotoing@gmail.com>
2022-12-10 00:32:31 -0500

    Add: new action to product

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,15 +5,22 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     get products_path
 
     assert_response :success
-    assert.assert_select '.product', 2
+    assert_select '.product', 2
   end
 
   test 'render a detail product page' do
     get product_path(products(:kodak))
 
     assert_response :success
-    assert.select '.title', 'Kodak'
-    assert.select '.description', 'Sony Cyber-shot DSC-RX1R II'
-    assert.select '.price', '3992'
+    assert_select '.title', 'Kodak'
+    assert_select '.description', 'Sony Cyber-shot DSC-RX1R II'
+    assert_select '.price', '$3992'
+  end
+
+  test 'Render new product form' do
+    get new_product_path
+
+    assert_response :success
+    assert_select 'form'
   end
 end
