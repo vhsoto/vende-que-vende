151eac903db231a09197e8f2dc2db301472d3f87
vhsoto <vhsotoing@gmail.com>
2023-01-03 11:58:32 -0500

    Feat: basic user authentication (NO DEVISE GEM)

---
+++ test/controllers/authentication/sessions_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class Authentication::SessionsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:pepito)
+  end
+  test "should get new" do
+    get new_session_url
+    assert_response :success
+  end
+
+  test "should login an user by email" do
+    post sessions_url, params: { login: @user.email, password: '123456' }
+
+    assert_redirected_to products_url
+  end
+
+  test "should login an user by username" do
+    post sessions_url, params: { login: @user.username, password: '123456' }
+
+    assert_redirected_to products_url
+  end
+end
